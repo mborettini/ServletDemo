22dc082beddb5d36022179b5b5fa767505144dfa
Magdalena Borettini <magdalena.borettini@gmail.com>
2020-07-19 11:07:42 +0200

    Part 1 -HelloServlet

---
+++ src/main/java/com/sda/AboutMeServlet.java
@@ -0,0 +1,7 @@
+package com.sda;
+
+import javax.servlet.http.HttpServlet;
+
+public class AboutMeServlet extends HttpServlet {
+
+}
---
+++ src/main/java/com/sda/ProgrammingLanguageDashboardServlet.java
@@ -0,0 +1,7 @@
+package com.sda;
+
+import javax.servlet.http.HttpServlet;
+
+public class ProgrammingLanguageDashboardServlet extends HttpServlet {
+
+}
